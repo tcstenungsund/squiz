f8ffaf489152db4e6d0f47751edb8737c80ff5ef
Nqtural <robin.stalstad@proton.me>
2025-12-16 16:09:44 +0100

    feat: store users' answer selection in browser storage

--- js/model/model.mjs
+++ js/model/model.mjs
@@ -1,7 +1,54 @@
+import { AnswerStore } from "./answerStore.mjs";
+
 export class Model {
 	constructor() {
 		this.questions = [];
 		this.currentIndex = 0;
+		this.answerStore = new AnswerStore();
+	}
+
+	saveCurrentAnswer(answer_index) {
+		const question = this.getCurrentQuestion();
+		const answer = question.answers[answer_index];
+		this.answerStore.setAnswer(question.id, answer_index, answer);
+	}
+
+	getCurrentAnswer() {
+		const question = this.getCurrentQuestion();
+		return this.answerStore.getAnswer(question.id);
+	}
+
+	getResults() {
+		const scores = {
+			tei: 0,
+			tet: 0,
+			ted: 0,
+			tes: 0,
+			tep: 0,
+			ind: 0,
+			indDrift: 0,
+			indProcess: 0,
+			indProduct: 0,
+			indSvets: 0,
+		};
+
+		for (const question of this.questions) {
+			const answer = this.answerStore.getAnswer(question.id);
+			if (!answer || !answer.score) continue;
+
+			for (const key in answer.score) {
+				if (key in scores) {
+					scores[key] += answer.score[key];
+				}
+			}
+		}
+
+		const max_score = Math.max(...Object.values(scores));
+		const result = Object.keys(scores).filter(
+			(key) => scores[key] === max_score && max_score > 0,
+		);
+
+		return result;
 	}
 
 	async loadQuestions() {
