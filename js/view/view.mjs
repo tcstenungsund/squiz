3ac501a9da4409808eca0b6f35739aaee9d1f985
WPK2006 <william06.a@gmail.com>
2026-01-09 00:32:08 +0100

    Button icons (#72)
    Co-authored-by: Nqtural <61121237+Nqtural@users.noreply.github.com>

--- js/view/view.mjs
+++ js/view/view.mjs
@@ -60,10 +60,7 @@ export class View {
 	renderPrevBtn(handler) {
 		this.container
 			.querySelector("#btn-container")
-			.insertAdjacentHTML(
-				"beforeend",
-				`<button id="prev-btn">Föregående</button>`,
-			);
+			.insertAdjacentHTML("beforeend", `<button id="prev-btn"></button>`);
 		this.container
 			.querySelector("#prev-btn")
 			.addEventListener("click", handler);
@@ -75,7 +72,7 @@ export class View {
 			.querySelector("#btn-container")
 			.insertAdjacentHTML(
 				"beforeend",
-				`<button id="next-btn" ${disabled_attr}>Nästa</button>`,
+				`<button id="next-btn" ${disabled_attr}></button>`,
 			);
 		this.container
 			.querySelector("#next-btn")
@@ -85,10 +82,7 @@ export class View {
 	renderSubmitBtn(handler) {
 		this.container
 			.querySelector("#btn-container")
-			.insertAdjacentHTML(
-				"beforeend",
-				`<button id="submit-btn">Slutför</button>`,
-			);
+			.insertAdjacentHTML("beforeend", `<button id="submit-btn"></button>`);
 		this.container
 			.querySelector("#submit-btn")
 			.addEventListener("click", handler);
